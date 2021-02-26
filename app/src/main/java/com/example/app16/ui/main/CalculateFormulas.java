d8de6f4f5f2e9758669841ccb8feadcaebae965f
Azky <azkymohamed@ymail.com>
2021-02-26 07:56:49 +0000

    File additions

---
+++ app/src/main/java/com/example/app16/ui/main/CalculateFormulas.java
@@ -0,0 +1,15 @@
+package com.example.app16.ui.main;
+/*
+This class is responsible for performing all the required calculations
+Methods to add SMA, EMA, MACD, MACDAVG
+
+ */
+public class CalculateFormulas {
+
+
+
+
+
+
+
+}
