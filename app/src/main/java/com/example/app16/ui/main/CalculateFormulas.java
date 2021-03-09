45e89703047c47da5b872bc18af30f053ebc55c0
suhailmir <suhabb@gmail.com>
2021-03-09 15:28:58 +0000

    EMA average:

--- app/src/main/java/com/example/app16/ui/main/CalculateFormulas.java
+++ app/src/main/java/com/example/app16/ui/main/CalculateFormulas.java
@@ -10,14 +10,15 @@ public class CalculateFormulas {
 
     static ArrayList<String> timeFrames;
     static ArrayList<Double> stockValues;
-    public CalculateFormulas(ArrayList[] timeFrameAndValues){
+
+    public CalculateFormulas(ArrayList[] timeFrameAndValues) {
         timeFrames = (ArrayList<String>) timeFrameAndValues[0];
         stockValues = (ArrayList<Double>) timeFrameAndValues[1];
     }
 
-    public ArrayList[] calcForInstrument(IndicatorsEnum type){
+    public ArrayList[] calcForInstrument(IndicatorsEnum type) {
 
-        switch (type){
+        switch (type) {
             case SMA:
                 return calculateSMA();
             case EMA:
@@ -33,40 +34,69 @@ public class CalculateFormulas {
     /*SMA calculation
     note: due to varying input range, Sma figures are based on a period of 5% of the timeframe
      */
-    private ArrayList[] calculateSMA(){
+    private ArrayList[] calculateSMA() {
         ArrayList lists[] = new ArrayList[2];
-        for(int i=0;i<2;i++){ lists[i]=new ArrayList<>(); }
+        for (int i = 0; i < 2; i++) {
+            lists[i] = new ArrayList<>();
+        }
         int smaPeriod = (int) (timeFrames.size() * 0.05);
         int count = smaPeriod;
-        while (count<timeFrames.size()){
+        while (count < timeFrames.size()) {
             lists[0].add(timeFrames.get(count));// prev : timeFrames.get(count)
-            lists[1].add(sumValues(count-smaPeriod,count)/smaPeriod);
-            count+=smaPeriod;
+            lists[1].add(sumValues(count - smaPeriod, count) / smaPeriod);
+            count += smaPeriod;
         }
         return lists;
     }
-    public ArrayList[] calculateEMA( ){
+    // EMA = (today’s closing price *K) + (Previous EMA * (1 – K))
+
+    //K (Smoothing Factor) = 2/(N+1)
+    public ArrayList[] calculateEMA() {
+
+        ArrayList lists[] = new ArrayList[2];
+        for (int i = 0; i < 2; i++) {
+            lists[i] = new ArrayList<>();
+        }
+        int emaPeriod = (int) (timeFrames.size() * 0.05);
+        int count = emaPeriod;
+        while (count < timeFrames.size()) {
+            lists[0].add(timeFrames.get(count));
+            lists[1].add(sumEMAvalues(count-emaPeriod,count));
+            count += emaPeriod;
+        }
+        return lists;
+    }
+
+    public ArrayList[] calculateMACD() {
 
 
         return null;
     }
-    public ArrayList[] calculateMACD( ){
 
-
-        return null;
-    }
-    public ArrayList[] calculateMACDAVG( ){
-
+    public ArrayList[] calculateMACDAVG() {
 
 
         return null;
     }
 
-    public double sumValues(int x, int y)
-    {
+    //EMA=Price(t)×k+EMA(y)×(1−k)
+    //=today
+    //y=yesterday
+    //N=number of days in EMA
+    //k=2÷(N+1)
+    //​
+    public double sumEMAvalues(int x, int numberOfDays) {
         double sum = 0;
-        for(int i = x; i < y; i++)
-        {
+        double k = (double) 2 / (numberOfDays + 1);
+        for (int i = x; i < numberOfDays; i++) {
+            sum += (stockValues.get(i) * k) + (stockValues.get(i + 1) * (1 - k));
+        }
+        return (sum);
+    }
+
+    public double sumValues(int x, int y) {
+        double sum = 0;
+        for (int i = x; i < y; i++) {
             sum += stockValues.get(i);
         }
         return (sum);
